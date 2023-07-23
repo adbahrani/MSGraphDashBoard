c96fc6e7382e38422820848076742f88e5a1193c
adbarani <adbarani2@gmail.com>
2023-07-23 14:36:57 -0400

    Remove Secret, add eslint ,prettier

--- client/src/authConfig.js
+++ client/src/authConfig.js
@@ -3,7 +3,7 @@
  * Licensed under the MIT License.
  */
 
-import { LogLevel } from "@azure/msal-browser";
+import { LogLevel } from '@azure/msal-browser'
 
 /**
  * Configuration object to be passed to MSAL instance on creation.
@@ -11,50 +11,48 @@ import { LogLevel } from "@azure/msal-browser";
  * https://github.com/AzureAD/microsoft-authentication-library-for-js/blob/dev/lib/msal-browser/docs/configuration.md
  */
 export const settings = {
-  clientId: "5107c906-38b3-4cef-b630-7fd01f814ed9",
-
-  clientSecret: "AaB8Q~dsf1cz-S77NUzfrK5.SKw2GKVTKsd4Vc_z",
-  tenantId: "a6dfed0e-808d-4a2e-ae4f-9adac874f50d",
-  secretId: "6fd170a8-5a08-4427-a306-8af59c6c7261",
-  secretValue: "_hh8Q~9m7BST~c_zvzkFSHfOtPayCJDT2wbxqc_O"
-};
+    clientId: '5107c906-38b3-4cef-b630-7fd01f814ed9',
+    clientSecret: 'AaB8Q~dsf1cz-S77NUzfrK5.SKw2GKVTKsd4Vc_z',
+    tenantId: 'a6dfed0e-808d-4a2e-ae4f-9adac874f50d',
+    secretId: '6fd170a8-5a08-4427-a306-8af59c6c7261',
+}
 
 export const msalConfig = {
-  auth: {
-    clientId: `${settings.clientId}`,
-    authority: `https://login.microsoftonline.com/${settings.tenantId}`,
-    redirectUri: "http://localhost:3000"
-  },
-  cache: {
-    cacheLocation: "sessionStorage", // This configures where your cache will be stored
-    storeAuthStateInCookie: false // Set this to "true" if you are having issues on IE11 or Edge
-  },
-  system: {
-    loggerOptions: {
-      loggerCallback: (level, message, containsPii) => {
-        if (containsPii) {
-          return;
-        }
-        // switch (level) {
-        //   case LogLevel.Error:
-        //     console.error(message);
-        //     return;
-        //   case LogLevel.Info:
-        //     console.info(message);
-        //     return;
-        //   case LogLevel.Verbose:
-        //     console.debug(message);
-        //     return;
-        //   case LogLevel.Warning:
-        //     console.warn(message);
-        //     return;
-        //   default:
-        //     return;
-        //}
-      }
-    }
-  }
-};
+    auth: {
+        clientId: `${settings.clientId}`,
+        authority: `https://login.microsoftonline.com/${settings.tenantId}`,
+        redirectUri: 'http://localhost:3000',
+    },
+    cache: {
+        cacheLocation: 'sessionStorage', // This configures where your cache will be stored
+        storeAuthStateInCookie: false, // Set this to "true" if you are having issues on IE11 or Edge
+    },
+    system: {
+        loggerOptions: {
+            loggerCallback: (level, message, containsPii) => {
+                if (containsPii) {
+                    return
+                }
+                // switch (level) {
+                //   case LogLevel.Error:
+                //     console.error(message);
+                //     return;
+                //   case LogLevel.Info:
+                //     console.info(message);
+                //     return;
+                //   case LogLevel.Verbose:
+                //     console.debug(message);
+                //     return;
+                //   case LogLevel.Warning:
+                //     console.warn(message);
+                //     return;
+                //   default:
+                //     return;
+                //}
+            },
+        },
+    },
+}
 
 /**
  * Scopes you add here will be prompted for user consent during sign-in.
@@ -63,18 +61,18 @@ export const msalConfig = {
  * https://docs.microsoft.com/en-us/azure/active-directory/develop/v2-permissions-and-consent#openid-connect-scopes
  */
 export const loginRequest = {
-  scopes: ["User.Read"]
-};
+    scopes: ['User.Read'],
+}
 
 /**
  * Add here the scopes to request when obtaining an access token for MS Graph API. For more information, see:
  * https://github.com/AzureAD/microsoft-authentication-library-for-js/blob/dev/lib/msal-browser/docs/resources-and-scopes.md
  */
 export const graphConfig = {
-  graphMeEndpoint: "https://graph.microsoft.com/v1.0/me",
-  application: "https://graph.microsoft.com/v1.0/applications",
-  usersEndGuest: `https://graph.microsoft.com/v1.0/users?$filter=usertype eq 'member'`,
-  usersEndMember: `https://graph.microsoft.com/v1.0/users?$filter=usertype eq 'guest'`,
-  users: "https://graph.microsoft.com/v1.0/users",
-  token: "/token"
-};
+    graphMeEndpoint: 'https://graph.microsoft.com/v1.0/me',
+    application: 'https://graph.microsoft.com/v1.0/applications',
+    usersEndGuest: `https://graph.microsoft.com/v1.0/users?$filter=usertype eq 'member'`,
+    usersEndMember: `https://graph.microsoft.com/v1.0/users?$filter=usertype eq 'guest'`,
+    users: 'https://graph.microsoft.com/v1.0/users',
+    token: '/token',
+}
--- server/server.ts
+++ server/server.ts
@@ -1,53 +1,55 @@
-import path from "path";
+import path from 'path'
 
-const { default: axios } = require("axios");
-const express = require("express");
-const cors = require("cors");
-var compression = require("compression");
+const { default: axios } = require('axios')
+const express = require('express')
+const cors = require('cors')
+var compression = require('compression')
+require('dotenv').config()
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5000
+console.log(process.env)
 
-const app = express();
+const app = express()
 
-app.use(compression());
-app.use(express.json());
-app.use(cors({ origin: "*" }));
+app.use(compression())
+app.use(express.json())
+app.use(cors({ origin: '*' }))
 
-app.use(express.static("client/build"));
+app.use(express.static('client/build'))
 
-app.get("/token", async (_, res) => {
-  const body = {
-    client_id: "5107c906-38b3-4cef-b630-7fd01f814ed9",
-    client_secret: "OZB8Q~l9fEVvsv06U49Cu5IxHjToqs1M2HodSaoG",
-    scope: "https://graph.microsoft.com/.default",
-    grant_type: "client_credentials"
-  };
+app.get('/token', async (_, res) => {
+    const body = {
+        client_id: process.env.Client_Id,
+        client_secret: process.env.Client_Secret,
+        scope: 'https://graph.microsoft.com/.default',
+        grant_type: 'client_credentials',
+    }
 
-  app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "../client/build/index.html"));
-  });
+    app.get('*', (req, res) => {
+        res.sendFile(path.join(__dirname, '../client/build/index.html'))
+    })
 
-  const formData = Object.entries(body)
-    .reduce((acc, [key, value]) => {
-      acc.push(`${key}=${encodeURIComponent(value)}`);
-      return acc;
-    }, [])
-    .join("&");
+    const formData = Object.entries(body)
+        .reduce((acc, [key, value]) => {
+            acc.push(`${key}=${encodeURIComponent(value)}`)
+            return acc
+        }, [])
+        .join('&')
 
-  const { data } = await axios.post(
-    "https://login.microsoftonline.com/a6dfed0e-808d-4a2e-ae4f-9adac874f50d/oauth2/v2.0/token",
-    formData,
-    { headers: { "Content-Type": "application/x-www-form-urlencoded" } }
-  );
+    const { data } = await axios.post(
+        'https://login.microsoftonline.com/a6dfed0e-808d-4a2e-ae4f-9adac874f50d/oauth2/v2.0/token',
+        formData,
+        { headers: { 'Content-Type': 'application/x-www-form-urlencoded' } }
+    )
 
-  res.send(data.access_token);
-});
+    res.send(data.access_token)
+})
 
 app.listen(PORT, function () {
-  console.log(`server started on port http://localhost:${PORT}`);
-  console.log(
-    "Express server listening on port %d in %s mode",
-    this.address().port,
-    app.settings.env
-  );
-});
+    console.log(`server started on port http://localhost:${PORT}`)
+    console.log(
+        'Express server listening on port %d in %s mode',
+        this.address().port,
+        app.settings.env
+    )
+})
