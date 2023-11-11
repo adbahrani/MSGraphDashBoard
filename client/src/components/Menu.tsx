ba063f33d0e3677c7077da5e139f8e3bb3981e61
natheraf <ma.eric11214@outlook.com>
2023-11-11 18:41:11 -0500

    added dropdown for data pages

--- client/src/components/Menu.tsx
+++ client/src/components/Menu.tsx
@@ -1,5 +1,9 @@
 import { useEffect, useState } from 'react'
-import { Link, useLocation } from 'react-router-dom'
+import { Link, useLocation, useNavigate } from 'react-router-dom'
+import InputLabel from '@mui/material/InputLabel';
+import MenuItem from '@mui/material/MenuItem';
+import FormControl from '@mui/material/FormControl';
+import Select, { SelectChangeEvent } from '@mui/material/Select';
 
 const linkStyle = { color: '#343434', textDecoration: 'none' }
 const activeLinkStyle = { ...linkStyle, backgroundColor: '#eee', borderRadius: '15px', padding: '10px' }
@@ -7,24 +11,37 @@ const links = [
     { title: 'Home', to: '/', activeSet: new Set(['/']) },
     { title: 'About', to: '/#about', activeSet: new Set(['/#about']) },
     { title: 'Services', to: '/#services', activeSet: new Set(['/#services']) },
+    { title: 'Contact', to: '/#contact', activeSet: new Set(['/#contact']) },
     { title: 'Users', to: '/users', activeSet: new Set(['/users']) },
     { title: 'Groups', to: '/groups', activeSet: new Set(['/groups', '/group']) },
     { title: 'Teams', to: '/teams', activeSet: new Set(['/teams']) },
     { title: 'SharePoint', to: '/sharepoint', activeSet: new Set(['/sharepoint']) },
     { title: 'OneDrive', to: '/onedrive', activeSet: new Set(['/onedrive']) },
     { title: 'Reports', to: '/reports', activeSet: new Set(['/reports']) },
-    { title: 'Contact', to: '/#contact', activeSet: new Set(['/#contact']) },
 ]
 
 export const Menu = () => {
-    const location = useLocation()
-    const [activeLink, setActiveLink] = useState('')
+    const location = useLocation();
+    const [activeLink, setActiveLink] = useState('');
+    const navigate = useNavigate();
+    const [currentPage, setCurrentPage] = useState('');
+    const isLinkAnchor = (linkTo: string) => linkTo === '/' || linkTo.length >= 2 && linkTo.substring(0, 2) === '/#';
+    
+    const handleChange = (event: SelectChangeEvent) => {
+        setCurrentPage(event.target.value);
+        navigate(event.target.value);
+    }
+    const clearDropdown = () => setCurrentPage('');
 
     useEffect(() => {
         const pathHash = `${location.pathname}${location.hash}`
         setActiveLink(links.find(link => link.activeSet.has(pathHash))?.to || '')
+        
+        if (!isLinkAnchor(pathHash)) {
+            setCurrentPage(links.find(link => link.activeSet.has(pathHash))?.to || '');
+        }
     }, [location])
-
+    
     return (
         <>
             <div
@@ -44,9 +61,9 @@ export const Menu = () => {
                 }}
             >
                 <div style={{ fontSize: '22px' }}>
-                    <Link to="/" style={linkStyle}>
+                    <Link to="/" style={linkStyle} onClick={clearDropdown}>
                         M365 Pulse
-                    </Link>
+                    </Link> 
                 </div>
                 <div>
                     <ul
@@ -57,13 +74,27 @@ export const Menu = () => {
                             gap: '16px',
                         }}
                     >
-                        {links.map(link => (
+                        {links.filter(link => isLinkAnchor(link.to)).map(link => (
                             <li key={link.title}>
-                                <Link to={link.to} style={activeLink === link.to ? activeLinkStyle : linkStyle}>
+                                <Link to={link.to} style={activeLink === link.to ? activeLinkStyle : linkStyle } onClick={clearDropdown}>
                                     {link.title}
                                 </Link>
                             </li>
                         ))}
+                        <FormControl sx={{minWidth: 150, maxHeight: 0, marginTop: -1}} size="small">
+                            <InputLabel id="dropdown-link-select- label" style={{color: 'black'}}>My Data</InputLabel>
+                            <Select 
+                                labelId="dropdown-link-select-label"
+                                id="dropdown-link-select"
+                                value={currentPage}
+                                label="MyData"
+                                onChange={handleChange}
+                            >
+                                {links.filter(link => !isLinkAnchor(link.to)).map(link => (
+                                    <MenuItem value={link.to}>{link.title}</MenuItem>
+                                ))}
+                            </Select>
+                        </FormControl>
                     </ul>
                 </div>
             </div>
