94ec649c336a527148f01e847cda0b70a9455559
natheraf <ma.eric11214@outlook.com>
2023-12-12 21:34:19 -0500

    added MUI and theme

--- client/src/views/Login.tsx
+++ client/src/views/Login.tsx
@@ -1,15 +1,39 @@
+import {
+    FormControl,
+    InputLabel,
+    OutlinedInput,
+    InputAdornment,
+    IconButton,
+    Button,
+    createTheme,
+    ThemeProvider,
+} from '@mui/material'
+import { Visibility, VisibilityOff } from '@mui/icons-material'
 import { useNavigate } from 'react-router-dom'
+import { useState, MouseEvent } from 'react'
 
 export const Login = () => {
-    const navigate = useNavigate()
-    const isSignedIn = () => (localStorage.getItem('email') ? true : false)
-
-    if (isSignedIn()) {
-        localStorage.clear()
+    const theme = createTheme({
+        spacing: 14,
+    })
+    const [showPassword, setShowPassword] = useState(false)
+    const handleClickShowPassword = () => setShowPassword(show => !show)
+    const handleMouseDownPassword = (event: MouseEvent<HTMLButtonElement>) => {
+        event.preventDefault()
     }
-    const handleLogin = (event: any) => {
-        const email = event?.target.elements.email.value
-        const password = event?.target.elements.password.value
+    const [password, setPassword] = useState('')
+    const handleOnChangePassword = (event: any) => {
+        setPassword(event.target.value)
+    }
+    const [email, setEmail] = useState('')
+    const handleOnChangeEmail = (event: any) => {
+        setEmail(event.target.value)
+    }
+    const navigate = useNavigate()
+    const handleLogin = () => {
+        if (!email || !password) {
+            return
+        }
         localStorage.setItem('email', email)
         localStorage.setItem('password', password)
         navigate('/')
@@ -17,31 +41,61 @@ export const Login = () => {
 
     return (
         <>
-            <section
-                id="home"
-                style={{
-                    height: '100vh',
-                    width: 'calc(100% - 10vw)',
-                    backgroundColor: 'lightgreen',
-                    display: 'flex',
-                    flexDirection: 'column',
-                    alignItems: 'center',
-                    justifyContent: 'center',
-                    padding: '0 5vw',
-                    textAlign: 'center',
-                }}
-            >
-                <h1>Login</h1>
-                <form id="form" onSubmit={handleLogin.bind(this)}>
+            <ThemeProvider theme={theme}>
+                <div
+                    id="home"
+                    style={{
+                        height: '100vh',
+                        backgroundColor: 'lightgreen',
+                        display: 'flex',
+                        flexDirection: 'column',
+                        alignItems: 'center',
+                        justifyContent: 'center',
+                        textAlign: 'center',
+                    }}
+                >
+                    <h1>Login</h1>
                     <div>
-                        Email: <input name="email" type="text"></input>
+                        <FormControl sx={{ m: 1, width: '25ch' }} variant="outlined">
+                            <InputLabel htmlFor="email">Email</InputLabel>
+                            <OutlinedInput
+                                id="email"
+                                type={'text'}
+                                label="Email"
+                                onChange={handleOnChangeEmail.bind(this)}
+                            />
+                        </FormControl>
                     </div>
                     <div>
-                        Password: <input name="password" type="password"></input>
+                        <FormControl sx={{ m: 1, width: '25ch' }} variant="outlined">
+                            <InputLabel htmlFor="password">Password</InputLabel>
+                            <OutlinedInput
+                                id="password"
+                                type={showPassword ? 'text' : 'password'}
+                                endAdornment={
+                                    <InputAdornment position="end">
+                                        <IconButton
+                                            aria-label="toggle password visibility"
+                                            onClick={handleClickShowPassword}
+                                            onMouseDown={handleMouseDownPassword}
+                                            edge="end"
+                                        >
+                                            {showPassword ? <VisibilityOff /> : <Visibility />}
+                                        </IconButton>
+                                    </InputAdornment>
+                                }
+                                label="Password"
+                                onChange={handleOnChangePassword.bind(this)}
+                            />
+                        </FormControl>
+                        <div>
+                            <Button variant="contained" onClick={handleLogin} color="success">
+                                Submit
+                            </Button>
+                        </div>
                     </div>
-                    <button type="submit">submit</button>
-                </form>
-            </section>
+                </div>
+            </ThemeProvider>
         </>
     )
 }
